84fcf4a7b044e611bd4633919457b1db54587634
mgrv <mohit.grover@sbab.se>
2019-10-10 10:35:37 +0200

    Initial commit

---
+++ src/main/java/se/sbab/awsdocsservice/AwsDocsServiceApplication.java
@@ -0,0 +1,13 @@
+package se.sbab.awsdocsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDocsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDocsServiceApplication.class, args);
+	}
+
+}
