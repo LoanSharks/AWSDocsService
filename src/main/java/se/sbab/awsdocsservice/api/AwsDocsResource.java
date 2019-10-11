65ca3a0598fb3b53c4af9e063477ec2060d8b5d7
Zoran Pribojevic <zoran.pribojevic@sbab.se>
2019-10-11 09:19:53 +0200

    cross origin settings

--- src/main/java/se/sbab/awsdocsservice/api/AwsDocsResource.java
+++ src/main/java/se/sbab/awsdocsservice/api/AwsDocsResource.java
@@ -3,6 +3,7 @@ package se.sbab.awsdocsservice.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.sbab.awsdocsservice.GetUserDocs;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController(value = "document")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AwsDocsResource {
 
     private final GetUserDocs getUserDocs;
